76f97f3588dce161b14ed8d7b766aae095f78dbc
Vânia Almeida <vania.almeida28@hotmail.com>
2025-02-08 18:59:30 -0300

    :label: create movie model

---
+++ domain/src/main/kotlin/com/vaniala/movies/domain/model/Movie.kt
@@ -0,0 +1,18 @@
+package com.vaniala.movies.domain.model
+
+data class Movie(
+    val adult: Boolean? = null,
+    val backdropPath: String? = null,
+    val genreIds: List<Long>? = null,
+    val id: Long? = null,
+    val originalLanguage: String? = null,
+    val originalTitle: String? = null,
+    val overview: String? = null,
+    val popularity: Double? = null,
+    val posterPath: String? = null,
+    val releaseDate: String? = null,
+    val title: String? = null,
+    val video: Boolean? = null,
+    val voteAverage: Double? = null,
+    val voteCount: Long? = null,
+)
