bbb3cd85cb6ca0586b1ae6ac9b9eb6044c21f8ec
Vânia Almeida <vania.almeida28@hotmail.com>
2025-02-08 19:06:27 -0300

    :sparkles: get movies

--- app/src/main/kotlin/com/vaniala/movies/ui/MoviesActivity.kt
+++ app/src/main/kotlin/com/vaniala/movies/ui/MoviesActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.paging.compose.collectAsLazyPagingItems
+import com.vaniala.movies.HomeViewModel
 import com.vaniala.movies.ui.theme.MoviesTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -20,30 +26,27 @@ class MoviesActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val viwModel: HomeViewModel by viewModels()
+
+            val moviesPaging = viwModel.getMovies().collectAsLazyPagingItems()
             MoviesTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding),
-                    )
+                    LazyColumn(modifier = Modifier.padding(innerPadding)) {
+                        items(moviesPaging.itemCount) {
+                            Box(modifier = Modifier.padding(5.dp)) {
+                                Text(
+                                    text = (moviesPaging[it]?.title ?: String()),
+                                    color = Color.Black,
+                                    modifier = Modifier
+                                        .fillParentMaxWidth()
+                                        .padding(8.dp),
+                                    textAlign = TextAlign.Center,
+                                )
+                            }
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier,
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MoviesTheme {
-        Greeting("Android")
     }
 }
