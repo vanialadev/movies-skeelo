c263e56edb4580fd5e15e375c5607c93df9f0d3a
Vânia Almeida <vania.almeida28@hotmail.com>
2025-02-07 23:17:37 -0300

    :lipstick: refactor position libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,18 +46,20 @@ android {
 }
 
 dependencies {
+    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.navigation)
+
+    androidTestImplementation(platform(libs.androidx.compose.bom))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
@@ -67,8 +69,6 @@ dependencies {
     ksp(libs.hilt.compiler)
 
     implementation(libs.coil)
-
-    implementation(libs.navigation)
 
     detektPlugins(libs.detekt.formatting)
 
