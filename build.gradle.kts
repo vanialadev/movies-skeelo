bbe5b5c00ee132611c9d938cea0684cd8dbbafe6
Vânia Almeida <vania.almeida28@hotmail.com>
2025-02-07 22:24:10 -0300

    :heavy_plus_sign: add detekt plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
-    alias(libs.plugins.ktlint)
+    alias(libs.plugins.ktlint) apply false
+    alias(libs.plugins.detekt) apply false
 }
