02fddaf1a60f2e69220187c1e504a9aee52308cd
Vânia Almeida <vania.almeida28@hotmail.com>
2025-02-08 18:47:05 -0300

    :construction: update movie service

--- data/src/main/kotlin/com/vaniala/movies/data/remote/service/MovieService.kt
+++ data/src/main/kotlin/com/vaniala/movies/data/remote/service/MovieService.kt
@@ -1,10 +1,12 @@
 package com.vaniala.movies.data.remote.service
 
-import com.vaniala.movies.data.remote.model.Response
+import com.vaniala.movies.data.remote.model.ApiResponse
+import com.vaniala.movies.data.remote.model.MovieResponse
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface MovieService {
 
     @GET("movie/popular?language=pt-BR")
-    suspend fun getMoviePopular(): Response
+    suspend fun getMoviePopular(@Query("page") page: Int): ApiResponse<MovieResponse>
 }
