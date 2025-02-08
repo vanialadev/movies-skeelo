5113395541f26541ff140d352c022a0670f30e39
Vânia Almeida <vania.almeida28@hotmail.com>
2025-02-08 19:44:20 -0300

    :lipstick: create movie bottom app bar

---
+++ app/src/main/kotlin/com/vaniala/movies/ui/MovieBottomAppBar.kt
@@ -0,0 +1,81 @@
+package com.vaniala.movies.ui
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import com.vaniala.movies.ui.theme.MoviesTheme
+
+@Composable
+fun MovieBottomAppBar(
+    selectedItem: BottomAppBarItem,
+    items: List<BottomAppBarItem>,
+    modifier: Modifier = Modifier,
+    onItemClick: (BottomAppBarItem) -> Unit = {},
+) {
+    BottomAppBar(
+        modifier,
+        containerColor = MaterialTheme.colorScheme.background,
+    ) {
+        items.forEach {
+            NavigationBarItem(
+                selected = it == selectedItem,
+                onClick = { onItemClick(it) },
+                icon = {
+                    Column {
+                        Icon(
+                            it.icon,
+                            contentDescription = null,
+                        )
+                    }
+                },
+                label = {
+                    Text(text = it.label)
+                },
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun MovieBottomAppBarPreview() {
+    MoviesTheme {
+        Surface {
+            MovieBottomAppBar(
+                selectedItem = BottomAppBarItem.Perfil,
+                items = listOf(
+                    BottomAppBarItem.Home,
+                    BottomAppBarItem.Perfil,
+                ),
+            )
+        }
+    }
+}
+
+sealed class BottomAppBarItem(val label: String, val icon: ImageVector) {
+    data object Home : BottomAppBarItem(
+        "Início",
+        Icons.Default.Home,
+    )
+
+    data object Perfil : BottomAppBarItem(
+        label = "Perfil",
+        icon = Icons.Filled.AccountCircle,
+    )
+}
+
+val bottomAppBarItems = listOf(
+    BottomAppBarItem.Home,
+    BottomAppBarItem.Perfil,
+)
--- app/src/main/kotlin/com/vaniala/movies/ui/MoviesActivity.kt
+++ app/src/main/kotlin/com/vaniala/movies/ui/MoviesActivity.kt
@@ -11,12 +11,15 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
 import com.vaniala.movies.HomeViewModel
+import com.vaniala.movies.domain.model.Movie
 import com.vaniala.movies.ui.theme.MoviesTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -30,21 +33,55 @@ class MoviesActivity : ComponentActivity() {
 
             val moviesPaging = viwModel.getMovies().collectAsLazyPagingItems()
             MoviesTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    LazyColumn(modifier = Modifier.padding(innerPadding)) {
-                        items(moviesPaging.itemCount) {
-                            Box(modifier = Modifier.padding(5.dp)) {
-                                Text(
-                                    text = (moviesPaging[it]?.title ?: String()),
-                                    color = Color.Black,
-                                    modifier = Modifier
-                                        .fillParentMaxWidth()
-                                        .padding(8.dp),
-                                    textAlign = TextAlign.Center,
-                                )
-                            }
-                        }
-                    }
+                MovieApp()
+            }
+        }
+    }
+}
+
+@Composable
+fun MovieApp(
+//    navController: NavHostController = rememberNavController()
+) {
+//    val backStackEntryAsState by navController.currentBackStackEntryAsState()
+//    val currentDestination = backStackEntryAsState?.destination
+
+//    val currentRout = currentDestination?.route
+}
+
+@Composable
+fun MovieApp(onBottomAppBarItemSelectedChange: (BottomAppBarItem) -> Unit = {}) {
+    Scaffold(
+        bottomBar = {
+            MovieBottomAppBar(
+                selectedItem = BottomAppBarItem.Home,
+                items = bottomAppBarItems,
+                onItemClick = onBottomAppBarItemSelectedChange,
+            )
+        },
+    ) { innerPadding ->
+        Box(
+            modifier = Modifier.padding(innerPadding),
+        ) {
+            Text(modifier = Modifier.padding(innerPadding), text = "teste")
+        }
+    }
+}
+
+@Composable
+fun Teste(moviesPaging: LazyPagingItems<Movie>) {
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        LazyColumn(modifier = Modifier.padding(innerPadding)) {
+            items(moviesPaging.itemCount) {
+                Box(modifier = Modifier.padding(5.dp)) {
+                    Text(
+                        text = (moviesPaging[it]?.title ?: String()),
+                        color = Color.Black,
+                        modifier = Modifier
+                            .fillParentMaxWidth()
+                            .padding(8.dp),
+                        textAlign = TextAlign.Center,
+                    )
                 }
             }
         }
