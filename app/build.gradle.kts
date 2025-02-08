0d690e48c99db8ec603131c75003d61ccfe3a0e6
Vânia Almeida <vania.almeida28@hotmail.com>
2025-02-07 22:40:47 -0300

    :heavy_plus_sign:  add detekt formatting

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    detektPlugins(libs.detekt.formatting)
 }
 detekt {
     buildUponDefaultConfig = true
