d61c25fd55fd376563d52ff0a8434fda78778a26
Vania Almeida <vania.almeida28@hotmail.com>
2025-02-09 19:32:54 -0300

    :recycle: add topbar and bottom bar only in activity

--- app/src/main/kotlin/com/vaniala/movies/ui/MoviesActivity.kt
+++ app/src/main/kotlin/com/vaniala/movies/ui/MoviesActivity.kt
@@ -4,19 +4,32 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.vaniala.movies.R
 import com.vaniala.movies.navigation.MovieNavHost
+import com.vaniala.movies.navigation.ScreensDestinations.HomeScreenDestination
 import com.vaniala.movies.navigation.ScreensDestinations.ProfileScreenDestination
 import com.vaniala.movies.navigation.navigateToBottomAppBarItem
 import com.vaniala.movies.ui.components.BottomAppBarItem
@@ -66,31 +79,81 @@ fun MovieApp(navController: NavHostController = rememberNavController()) {
         mutableStateOf(item)
     }
 
+    val isShowBackNavigation = when (currentRoute) {
+        HomeScreenDestination.route, ProfileScreenDestination.route -> false
+        else -> false
+    }
+    val isShowBottomAppBar = when (currentRoute) {
+        HomeScreenDestination.route, ProfileScreenDestination.route -> true
+        else -> false
+    }
     MovieApp(
         bottomAppBarItemSelected = selectedBottomAppBarItem,
+        isShowBackNavigation = isShowBackNavigation,
+        isShowBottomAppBar = isShowBottomAppBar,
         bottomAppBarItems = bottomAppBarItems,
+        onBackNavigationClick = {
+            navController.popBackStack()
+        },
         onBottomAppBarItemSelectedChange = { item ->
             navController.navigateToBottomAppBarItem(item)
+        },
+        isShowTopBar = isShowBottomAppBar,
+        topAppBarTitle = {
+            when (currentRoute) {
+                ProfileScreenDestination.route -> Text(stringResource(R.string.profile_title))
+            }
         },
     ) {
         MovieNavHost(navController = navController)
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MovieApp(
     bottomAppBarItems: List<BottomAppBarItem>,
+    topAppBarTitle: @Composable () -> Unit = {},
+    onBackNavigationClick: () -> Unit = {},
+    isShowBackNavigation: Boolean = false,
+    isShowBottomAppBar: Boolean = false,
     bottomAppBarItemSelected: BottomAppBarItem = bottomAppBarItems.first(),
+    isShowTopBar: Boolean = false,
     onBottomAppBarItemSelectedChange: (BottomAppBarItem) -> Unit = {},
     content: @Composable () -> Unit,
 ) {
     Scaffold(
+        topBar = {
+            if (isShowTopBar) {
+                TopAppBar(
+                    title = {
+                        topAppBarTitle()
+                    },
+                    navigationIcon = {
+                        if (isShowBackNavigation) {
+                            Icon(
+                                Icons.AutoMirrored.Filled.ArrowBack,
+                                contentDescription = null,
+                                Modifier
+                                    .padding(16.dp)
+                                    .clip(CircleShape)
+                                    .clickable {
+                                        onBackNavigationClick()
+                                    },
+                            )
+                        }
+                    },
+                )
+            }
+        },
         bottomBar = {
-            MovieBottomAppBar(
-                selectedItem = bottomAppBarItemSelected,
-                items = bottomAppBarItems,
-                onItemClick = onBottomAppBarItemSelectedChange,
-            )
+            if (isShowBottomAppBar) {
+                MovieBottomAppBar(
+                    selectedItem = bottomAppBarItemSelected,
+                    items = bottomAppBarItems,
+                    onItemClick = onBottomAppBarItemSelectedChange,
+                )
+            }
         },
     ) { innerPadding ->
         Box(
--- app/src/main/kotlin/com/vaniala/movies/ui/utils/Constants.kt
+++ app/src/main/kotlin/com/vaniala/movies/ui/utils/Constants.kt
@@ -3,4 +3,5 @@ package com.vaniala.movies.ui.utils
 object Constants {
 
     const val IMAGE_URL = "https://image.tmdb.org/t/p/w500"
+    const val GRAVATAR_URL = "https://gravatar.com/avatar/"
 }
