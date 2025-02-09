e5cd63e524bfbf795f3220aed0f5a8d337513099
Vânia Almeida <vania.almeida28@hotmail.com>
2025-02-09 14:53:30 -0300

    :lipstick: create tet style title

---
+++ app/src/main/kotlin/com/vaniala/movies/ui/theme/TextStyle.kt
@@ -0,0 +1,10 @@
+package com.vaniala.movies.ui.theme
+
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+val titleSection = TextStyle(
+    fontSize = 24.sp,
+    fontWeight = FontWeight.Medium,
+)
