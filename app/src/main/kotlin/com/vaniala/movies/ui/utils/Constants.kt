128ef1e396bfa21c0a427294f04842cb654e47dc
Vânia Almeida <vania.almeida28@hotmail.com>
2025-02-10 09:20:05 -0300

    :speech_balloon:  create image url constants

--- app/src/main/kotlin/com/vaniala/movies/ui/utils/Constants.kt
+++ app/src/main/kotlin/com/vaniala/movies/ui/utils/Constants.kt
@@ -2,7 +2,9 @@ package com.vaniala.movies.ui.utils
 
 object Constants {
 
-    const val IMAGE_URL = "https://image.tmdb.org/t/p/w500"
+    const val IMAGE_URL_SMALL = "https://image.tmdb.org/t/p/w500"
+    const val IMAGE_URL_LARGE = "https://image.tmdb.org/t/p/w780"
+    const val IMAGE_URL_ORIGINAL = "https://image.tmdb.org/t/p/original"
     const val GRAVATAR_URL = "https://gravatar.com/avatar/"
 
     const val SINGLE_COLUMN_MAX = 3
