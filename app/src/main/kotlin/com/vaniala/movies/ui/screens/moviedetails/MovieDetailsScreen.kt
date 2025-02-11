a8a1211dca87af0719def0976de37ba72efdf92a
Vânia Almeida <vania.almeida28@hotmail.com>
2025-02-10 23:56:53 -0300

    :lipstick: change details

--- app/src/main/kotlin/com/vaniala/movies/ui/screens/moviedetails/MovieDetailsScreen.kt
+++ app/src/main/kotlin/com/vaniala/movies/ui/screens/moviedetails/MovieDetailsScreen.kt
@@ -1,13 +1,17 @@
 package com.vaniala.movies.ui.screens.moviedetails
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
@@ -16,6 +20,7 @@ import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.outlined.Add
 import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -24,22 +29,27 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.ColorPainter
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.vaniala.movies.R
+import com.vaniala.movies.domain.model.Genres
 import com.vaniala.movies.domain.model.MovieAllDetails
 import com.vaniala.movies.domain.model.MovieDetails
-import com.vaniala.movies.domain.model.MovieStatus
-import com.vaniala.movies.ui.components.AddStatusButton
 import com.vaniala.movies.ui.components.ToggleButton
 import com.vaniala.movies.ui.utils.Constants.IMAGE_URL_ORIGINAL
+import kotlin.random.Random
 
+// todo:v refactor cirar componentes
+@Suppress("LongMethod")
 @Composable
 fun MovieDetailsScreen(
     uiState: MovieDetailsUiState,
@@ -54,12 +64,6 @@ fun MovieDetailsScreen(
                 .fillMaxSize()
                 .verticalScroll(rememberScrollState()),
         ) {
-            Text(
-                movieDetails?.title ?: String(),
-                style = MaterialTheme.typography.titleLarge,
-                textAlign = TextAlign.Center,
-                modifier = Modifier.fillMaxWidth(),
-            )
             AsyncImage(
                 ImageRequest.Builder(LocalContext.current)
                     .data("$IMAGE_URL_ORIGINAL${movieDetails?.backdropPath}")
@@ -74,36 +78,42 @@ fun MovieDetailsScreen(
                 placeholder = ColorPainter(Color.Gray),
                 contentScale = ContentScale.Crop,
             )
-            Column(
+            Row(
                 modifier = Modifier
+                    .fillMaxWidth()
                     .padding(16.dp),
+                verticalAlignment = Alignment.CenterVertically,
             ) {
-                MovieStatusDetails(
-                    movieStatus = movieStatus,
-                    toggleFavorite = toggleFavorite,
-                    toggleWatchlist = toggleWatchlist,
+                AsyncImage(
+                    ImageRequest.Builder(LocalContext.current)
+                        .data("$IMAGE_URL_ORIGINAL${movieDetails?.posterPath}")
+                        .placeholder(R.drawable.placeholder)
+                        .error(R.drawable.error)
+                        .build(),
+                    contentDescription = null,
+                    Modifier
+                        .width(100.dp)
+                        .height(150.dp),
+                    placeholder = ColorPainter(Color.Gray),
+                    contentScale = ContentScale.Crop,
                 )
+                Spacer(modifier = Modifier.width(16.dp))
+                Column {
+                    Text(text = movieDetails?.title ?: "", fontSize = 18.sp, fontWeight = FontWeight.Bold)
+                    Text(text = movieDetails?.tagline ?: "", fontSize = 14.sp)
+                    Text(
+                        text = stringResource(
+                            R.string.votes,
+                        ) + " ${movieDetails?.voteAverage} (${movieDetails?.voteCount} votes)",
+                        fontSize = 12.sp,
+                    )
+                }
             }
-        }
-    }
-}
 
-@Composable
-fun MovieStatusDetails(
-    movieStatus: MovieStatus?,
-    toggleFavorite: (Boolean) -> Unit = {},
-    toggleWatchlist: (Boolean) -> Unit = {},
-) {
-    Row {
-        Column(
-            verticalArrangement = Arrangement.spacedBy(4.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
             val toggleButtonFavorite =
                 if (movieStatus?.favorite == true) {
                     ToggleButton(
                         icon = Icons.Filled.Favorite,
-                        text = stringResource(R.string.remove_favorites),
                         action = {
                             toggleFavorite(false)
                         },
@@ -111,7 +121,6 @@ fun MovieStatusDetails(
                 } else {
                     ToggleButton(
                         icon = Icons.Outlined.FavoriteBorder,
-                        text = stringResource(R.string.favorite),
                         action = {
                             toggleFavorite(true)
                         },
@@ -121,7 +130,6 @@ fun MovieStatusDetails(
                 if (movieStatus?.watchlist == true) {
                     ToggleButton(
                         icon = Icons.Filled.Clear,
-                        text = stringResource(R.string.remove_watchlist),
                         action = {
                             toggleWatchlist(false)
                         },
@@ -129,20 +137,66 @@ fun MovieStatusDetails(
                 } else {
                     ToggleButton(
                         icon = Icons.Outlined.Add,
-                        text = stringResource(R.string.watchlist),
                         action = {
                             toggleWatchlist(true)
                         },
                     )
                 }
-            AddStatusButton(toggleButtonFavorite, onClick = {
-                toggleButtonFavorite.action()
-            })
-            AddStatusButton(toggleButtonWatchlist, onClick = {
-                toggleButtonWatchlist.action()
-            })
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                horizontalArrangement = Arrangement.SpaceAround,
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Row(
+                    modifier = Modifier.clickable {
+                        toggleButtonFavorite.action()
+                    },
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Icon(
+                        toggleButtonFavorite.icon,
+                        contentDescription = null,
+                        Modifier.size(43.dp),
+                        tint = MaterialTheme.colorScheme.primary,
+                    )
+                    Text(text = stringResource(R.string.favorites), color = MaterialTheme.colorScheme.primary)
+                }
+                Row(
+                    modifier = Modifier.clickable {
+                        toggleButtonWatchlist.action()
+                    },
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Icon(
+                        toggleButtonWatchlist.icon,
+                        contentDescription = null,
+                        Modifier.size(43.dp),
+                        tint = MaterialTheme.colorScheme.primary,
+                    )
+                    Text(text = stringResource(R.string.watchlist), color = MaterialTheme.colorScheme.primary)
+                }
+            }
+            Column(modifier = Modifier.padding(16.dp)) {
+                Text(text = stringResource(R.string.genres), fontSize = 16.sp, fontWeight = FontWeight.Bold)
+                movieDetails?.genres?.let { genres ->
+                    Text(text = genres.joinToString { it.name ?: "" })
+                }
+            }
+            Column(modifier = Modifier.padding(16.dp)) {
+                Text(text = stringResource(R.string.overview), fontSize = 16.sp, fontWeight = FontWeight.Bold)
+                Text(text = movieDetails?.overview ?: "")
+            }
+
+            Column(modifier = Modifier.padding(16.dp)) {
+                Text(text = "Recomendacoes", fontSize = 16.sp, fontWeight = FontWeight.Bold)
+
+            }
         }
     }
+
+    class ToggleButton(val icon: ImageVector, val action: () -> Unit)
 }
 
 @Preview(showBackground = true)
@@ -153,6 +207,10 @@ private fun MovieDetailsScreenPreview() {
             movieAllDetails = MovieAllDetails(
                 movieDetails = MovieDetails(
                     title = "Sonic 3",
+                    genres = listOf(Genres(id = 1, name = "Drama"), Genres(id = 2, name = "comedia")),
+                    overview = LoremIpsum(100).values.joinToString(" "),
+                    voteAverage = Random.nextDouble(0.0, 10.0),
+                    voteCount = Random.nextInt(100, 10000),
                 ),
             ),
         ),
